c7276b6a6cb971cc77085bbae8bc5527b311b33e
Steve Klebanoff <steve.klebanoff@mycase.com>
2017-05-23 17:29:54 -0700

    initial project with websocket dependency

---
+++ lib/arbie.ex
@@ -0,0 +1,18 @@
+defmodule Arbie do
+  @moduledoc """
+  Documentation for Arbie.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Arbie.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/arbie_test.exs
@@ -0,0 +1,8 @@
+defmodule ArbieTest do
+  use ExUnit.Case
+  doctest Arbie
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
